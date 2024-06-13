fd5ec7064e648862e4407e7cefe68acd87c47ac1
naumaanansari <nomaanansari1115@gmail.com>
2024-06-13 23:45:14 +0530

    Part - 2 : Added Routing (Login,Sign Up,Home Pages etc.)

---
+++ src/pages/Loginpage.js
@@ -0,0 +1,11 @@
+import Login from "../features/auth/components/Login";
+
+function Loginpage() {
+  return (
+    <div className="">
+        <Login/>
+    </div>
+  );
+}
+
+export default Loginpage;
---
+++ src/pages/Signuppage.js
@@ -0,0 +1,9 @@
+import Signup from "../features/auth/components/Signup";
+
+function Signuppage() {
+    return ( <div className="">
+        <Signup/>
+    </div> );
+}
+
+export default Signuppage;
